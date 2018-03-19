991f06ea721c23efdad82f92a7d71537c7c35009
avinashse <kumaravinashse@gmail.com>
2018-03-19 10:12:59 +0530

    adding check between file count and thread count
    If the no of files are less than 5 then initiate threads equal to the number of files.

--- src/readFileForLoaderStatement.py
+++ src/readFileForLoaderStatement.py
@@ -5,12 +5,13 @@ from QueueReaderThread import *
 THREAD_COUNT = 5
 
 if __name__ == '__main__':
-    with open("file", "r") as fp:
-        fileLists = fp.readlines()
+        with open("file", "r") as fp:
+                fileLists = fp.readlines()
 
-    for threadId in range(THREAD_COUNT):
-        ReaderThread(threadId, "Thread_" + str(threadId)).start()
+        THREAD_COUNT = len(fileLists) if len(fileLists) < THREAD_COUNT else THREAD_COUNT
 
-    for files in fileLists:
-        fileQueue.put(files)
+        for threadId in range(THREAD_COUNT):
+                ReaderThread(threadId, "Thread_" + str(threadId)).start()
 
+        for files in fileLists:
+                fileQueue.put(files)
